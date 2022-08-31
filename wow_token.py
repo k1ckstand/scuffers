6e2bfed98ebbe637eb3d6b5750d262e1d0d896ff
Brian Bashor <brian.bashor@gmail.com>
2022-08-31 10:28:10 -0500

    minor refactor

--- wow_token.py
+++ wow_token.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 from blizzard import Blizzard
-import pymongo
+from pymongo import MongoClient
 
 def main() -> None:
-    wow_token = blizzard.wow_token()
-    del wow_token['_links']
-    if not collection.find_one({'last_updated_timestamp': wow_token['last_updated_timestamp']}):
-        collection.insert_one(wow_token)
+    try:
+        wow_token = blizzard.wow_token()
+        del wow_token['_links']
+        if not collection.find_one({'last_updated_timestamp': wow_token['last_updated_timestamp']}):
+            collection.insert_one(wow_token)
+            print('DONE!')
+    except ValueError as v:
+        print('BAD REQUEST')
+        print(v)
+    except Exception as e:
+        print('Something went wrong ¯\_(⊙︿⊙)_/¯')
+        print(e)
 
 if __name__ == '__main__':
     blizzard = Blizzard()
 
-    my_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    my_client = MongoClient('mongodb://localhost:27017/')
     db = my_client['scuffers']
     collection = db['wow_token']
 

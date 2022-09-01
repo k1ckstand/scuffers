fb39500a20671187481b9beabcb0585dd6e2fbfa
Brian Bashor <brian.bashor@gmail.com>
2022-08-31 19:25:08 -0500

    fixed stuff

--- auction_house.py
+++ auction_house.py
@@ -1,53 +1,40 @@
 #!/usr/bin/env python3
 
 from pymongo import MongoClient
-from PIL import Image
 from datetime import datetime
 from blizzard import Blizzard
-from time import sleep
-import base64
-import requests
-
-blizzard = Blizzard()
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['scuffers']
-
-def log_me() -> None:
-    pass # @TODO
 
 def get_ah_data(realm_id: int=57) -> set:
     current_time = datetime.now()
     ah_data = blizzard.auction_house(realm_id)
     for item in ah_data['auctions']:
         item['date_time'] = current_time
-        db['auction_house_data'].insert_one(item)
+        db['auction_house'].insert_one(item)
 
-    for item_id in db['auction_house_data'].find({}).distinct('item.id'):
-        print(f'Getting item for {item_id}')
+    new_items = db['auction_house'].find({}).distinct('item.id')
+    total = len(new_items) - len(db['items'].find({}).distinct('id'))
+    i = 0
+    for item_id in new_items:
         if not db['items'].find_one({'id': item_id}):
-            item = blizzard.game_item(item_id)
-            item['date_time'] = current_time
-            db['items'].insert_one(item)
-            sleep(.5)
-
-    for item_id in db['items'].find({}).distinct('id'):
-        item = db['items'].find_one({ 'id': item_id })
-        if item:
-            image_url = item['media']['key']['href']
-            image_url = blizzard.generic_call(image_url)
-            image_url = image_url['assets'][0]['value']
-            r = requests.get(image_url)
-            image_b64 = r.content
-            if r.status_code == 200:
-                print(f'Getting icon for {item_id}')
-                insert_image = {
-                    'date_time': current_time,
-                    'id': item_id,
-                    'image': image_b64
-                }
-            db['media'].insert_one(insert_image)
-            sleep(.5)
+            print(f'{i}/{total}: Item: {item_id}')
+            i += 1
+            item = blizzard.item(item_id)
+            try:
+                item['date_time'] = current_time
+                image_url = item['media']['key']['href']
+                image_url = blizzard.generic_call_with_token(image_url)
+                image_url = image_url['assets'][0]['value']
+                image = blizzard.generic_call_without_token(image_url)
+                if image is not None:
+                    item['image'] = image
+                    db['items'].insert_one(item)
+            except Exception as e:
+                print(f'There was a problem with item: {item_id}')
+                print(e)
 
 if __name__ == '__main__':
+    blizzard = Blizzard()
     get_ah_data()
